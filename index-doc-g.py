5cbe834198f014d3a7b1bf08f1eee1f6e941150c
022738 <matt_cheung@instance-1.c.km-gpt.internal>
2023-07-05 17:06:57 +0000

    index doc changed to handle longer input and convet files

--- index-doc-g.py
+++ index-doc-g.py
@@ -3,6 +3,7 @@ import os.path
 from langchain.llms import VertexAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import GoogleDriveLoader
+from langchain.document_loaders import UnstructuredFileIOLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import VertexAIEmbeddings
 from langchain.vectorstores import Chroma
@@ -23,56 +24,56 @@ logging.basicConfig(filename='output.txt', level=logging.INFO, format='')
 embeddings = VertexAIEmbeddings()
 docs_to_index=[]
 if os.path.exists('/home/matt_cheung/.credentials/keys.json'):
-    print("setting up creds")    
     creds = service_account.Credentials.from_service_account_file('/home/matt_cheung/.credentials/keys.json', scopes=scopes)
-#get the document ids to train the model
-    print("create service")
-    service = build('sheets', 'v4', credentials=creds)
-    print("create drive service")
-    driveservice = build("drive", "v3", credentials=creds)
+    try:
+        #get the document ids to train the model
+        service = build('sheets', 'v4', credentials=creds)
+        driveservice = build("drive", "v3", credentials=creds)
         
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                     range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
 
-    if not values:
-        print('No data found.')
+        if not values:
+            print('No data found.')
         print('Received these documents to index')
-    for row in values:
-        # Print columns A and E, which correspond to indices 0 and 4.
-        # Check that service agent can access
-        print('processing %s', row[0])
-        try:
-            file = driveservice.files().get(fileId=row[0], supportsAllDrives=True).execute()
-            print(HttpError)
+        for row in values:
             # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s' % (row[0]))
-            docs_to_index.append(row[0])                
-        except HttpError as e:
-            if e.resp.status == 404:
-                print("File not found: {}".format(row[0]))
-            else:
-                print("An error occurred: {}".format(e))
+            # Check that service agent can access
+            try:
+                file = driveservice.files().get(fileId=row[0], supportsAllDrives=True).execute()
+                # Print columns A and E, which correspond to indices 0 and 4.
+                print('%s' % (row[0]))
+                docs_to_index.append(row[0])                
+            except HttpError as e:
+                if e.resp.status == 404:
+                    print("File not found: {}".format(row[0]) )
+                    logging.error("File not found: {}".format(row[0]))
+                else:
+                    print("An error occurred: {}".format(e))
+                    logging.error("An error occured:{}".format(e))
             
 
     
-#get the contents of the documents
-loader = GoogleDriveLoader(
-   document_ids=docs_to_index
-   )
-docs = loader.load()
-#split the docs into chunks
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=4000, chunk_overlap=0, separators=[" ", ",", "\n"]
-     )
-texts = text_splitter.split_documents(docs)
+    #get the contents of the documents
+        loader = GoogleDriveLoader(document_ids=docs_to_index, file_loader_cls=UnstructuredFileIOLoader,
+    file_loader_kwargs={"mode": "elements"})
+        docs = loader.load()
+        #split the docs into chunks
+        text_splitter = RecursiveCharacterTextSplitter(
+                  chunk_size=4000, chunk_overlap=0, separators=[" ", ",", "\n"] )
+        texts = text_splitter.split_documents(docs)
 
-embeddings = VertexAIEmbeddings()
-db =Chroma.from_documents(documents=texts, embedding=embeddings, persist_directory=persist_directory) 
+        embeddings = VertexAIEmbeddings()
+        db =Chroma.from_documents(documents=texts, embedding=embeddings, persist_directory=persist_directory) 
 
-print("completed loading", db)
+    except HttpError as err:
+        print(err)
 
+    print("completed loading", db)
+else:
+    print("Couldn't initialise")
 
 logging.info('Completed build')
