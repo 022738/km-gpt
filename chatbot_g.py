568426668372b3936e22b5265ae0ecf1ca3f61f1
022738 <matt_cheung@instance-1.c.km-gpt.internal>
2023-07-05 19:30:07 +0000

    better document chain handling

--- chatbot_g.py
+++ chatbot_g.py
@@ -1,11 +1,14 @@
 from langchain.llms import VertexAI
-from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders import GoogleDriveLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import VertexAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.chains import LLMChain
+from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
 
 persist_directory = 'gvectordb'
 #folder_id = "1HkHnvAYonewerZRWD0vDNf5HemeXbDjg"
@@ -17,8 +20,9 @@ print("I do think I have a db", db)
 retriever = db.as_retriever()
 
 llm = VertexAI(temperature=0)
-qa = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever,
-        memory=memory, return_source_documents=True)
+question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
+qa_chain = load_qa_with_sources_chain(llm, chain_type="stuff")
+qa = ConversationalRetrievalChain( retriever=retriever,question_generator=question_generator,memory=memory, combine_docs_chain=qa_chain, return_source_documents=True)
 
 #while True:
 #    query = input("> ")
@@ -39,8 +43,9 @@ def chatbot_g (query):
             answer = "I've wiped our conversation"
         else:
             try:
-                answer = qa({"question": query, "chat_history": ConversationBufferMemory()})
-                formattedanswer = answer['answer'] + "\nTitle: " +  answer['source_documents'][0].metadata['title'] + "\nFile url: " +  answer['source_documents'][0].metadata['source'] 
+                answer = qa({"question": query})
             except Exception as err:
                 answer = "An error occurred. Context: "+err.message
+        if len(answer['source_documents'])>0:    
+            answer = answer['answer'] + "\nTitle: " +  answer['source_documents'][0].metadata['title'] + "\nFile url: " +  answer['source_documents'][0].metadata['source'] 
         return answer
